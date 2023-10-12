667f0c9d5711c9a8d227544a1561896d3bce7802
luiz veronez <luizfelipemin120@hotmail.com>
2023-10-12 18:30:43 -0300

    aula 04

---
+++ src/main/java/br/com/luizveronez/todolist/filter/filterTaskAuth.java
@@ -0,0 +1,5 @@
+package br.com.luizveronez.todolist.filter;
+
+public class filterTaskAuth {
+    
+}
--- src/main/java/br/com/luizveronez/todolist/task/taskModel.java
+++ src/main/java/br/com/luizveronez/todolist/task/taskModel.java
@@ -22,4 +22,5 @@ public class taskModel {
     private String priority;
     private UUID idUser;
     
+    
 }
